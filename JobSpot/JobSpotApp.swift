16196f86c5f1738e9440b1f02f1c13def9639663
vikramsingh <vc7662@gmail.com>
2025-07-02 12:38:48 +0530

    Initial Commit

---
+++ JobSpot/JobSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSpotApp.swift
+//  JobSpot
+//
+//  Created by Vikram Singh Depawat on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JobSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
